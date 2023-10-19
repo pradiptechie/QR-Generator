fa52aa1bd6d6de3d857274bb8f61e6d8196899b3
pradiptechie <kumarpradip3956@gmail.com>
2023-10-19 16:40:10 +0545

    initial

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const  express = require('express')
 const app = express()
 const port = 3000;
 
+app.set("view engine", "hbs");
+
 app.get('/', (req, res) =>{ 
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.render("home");
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
-})
+});
+
